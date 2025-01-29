3729b079ba477a13b821062ee960432576d9da99
timusss0 <mustikavia86@gmail.com>
2025-01-29 09:04:06 +0700

    facility table & model

--- app/Models/Facility.php
+++ app/Models/Facility.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Facility extends Model
 {
-    //
+    use HasFactory , SoftDeletes;
+    protected $fillable = [
+        'image',
+        'name',
+        'description'
+    ];
 }
--- database/migrations/2025_01_29_004043_create_facilities_table.php
+++ database/migrations/2025_01_29_004043_create_facilities_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('facilities', function (Blueprint $table) {
             $table->id();
+            $table->string('image');
+            $table->string('name');
+            $table->string('description');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
