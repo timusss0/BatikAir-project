e8604d6fbe250893d1d6fa6c11699f696c55abad
timusss0 <mustikavia86@gmail.com>
2025-01-29 09:36:21 +0700

    transactionpassaneger

--- app/Models/TransactionPassenger.php
+++ app/Models/TransactionPassenger.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class TransactionPassenger extends Model
 {
-    //
+    use HasFactory , SoftDeletes;
+
+    protected $fillable = [
+        'transaction_id',
+        'flight_seat_id',
+        'name',
+        'date_of_birth',
+        'nationality'  
+    ];
 }
